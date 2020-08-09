b0695b12a1511027a8f375e0f09c13772b55d881
Noy Agasi <agasinoy@gmail.com>
2020-08-09 14:17:57 +0300

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,22 +33,31 @@ const IndexPage = () => (
         link="https://www.ceva-dsp.com/"
         desc="Software and Automation Developer, 2018 - Present"
       />
-      <p className={classes.desc}>- Developed some basic React web applications that display our failures and results. Based on Ant Design on frontend, REST/GraphQL and Node.js using “<a className={classes.linkText} href={"https://github.com/goldbergyoni/nodebestpractices"}>Best Practices</a>”.</p>
-      <p className={classes.desc}>- Implemented from scratch the Jython API that is used for IDE scripting in order to test our IDE automatically and expose bugs as quickly as possible.</p>
-      <p className={classes.desc}>- Debugged test failures across various platforms (Red Hat, Ubuntu and Windows) and repaired complex software bugs.</p>
-      <p className={classes.desc}>- Initiated independently Python scripting to improve efficiencies of CEVA's main product. It revealed plenty of performance slowdowns and saved us a lot of development time.</p>
+      <p className={classes.desc}>●	Developed some basic React web applications that used by our all department. Based on Ant Design on frontend, REST/GraphQL and Node.js using “<a className={classes.linkText} href={"https://github.com/goldbergyoni/nodebestpractices"}>Best Practices</a>”.</p>
+      <p className={classes.desc}>●	Implemented from scratch our IDE scripting API that led to almost maximum test coverage and saved 120% of daily working time for 4 automation engineers.</p>
+      <p className={classes.desc}>●	Built test automation that found significant bugs automatically before releases and helped us fixing complex bugs efficiently and quickly.</p>
+      <p className={classes.desc}>●	Initiated independently Python scripts to improve efficiencies of CEVA's main product. It revealed plenty of performance slowdowns and saved us a lot of development time.</p>
     </Section>
     <Section id="projects" title="Projects">
       <Link
         title="Goalaso Store"
         link="http://www.goalaso.store/"
-        desc="Ecommerce store based on CSS3 (Grid and Flexbox), Material-UI, React, Redux with action creators and React Hooks on client, Node.js with Express, Bcrypt, authorization and JWT authentication on server and AWS with EC2 and Route 53. Tested by Mocha, Chai and SinonJS."
+        desc="React.js, Redux, Node.js, Express.js, EC2 (AWS)"
       />
+      <p className={classes.desc}>●	Created an Ecommerce store based on a Node+Express back-end and hosted on AWS EC2.</p>
+      <p className={classes.desc}>●	Utilized Bcrypt and JWT Authentication for secure password creation and encryption.</p>
+      <p className={classes.desc}>●	Authored powerful unit tests with with Mocha, Chai, and SinonJS to ensure strong project release.</p>
+      <p className={classes.desc}>●	Constructed a React and Material-UI front-end, paired with CSS Grid and Flexbox to incorporate responsive and dynamic webapges.</p>
+      
       <Link
         title="Notes Application"
         link="https://noyag.herokuapp.com/"
-        desc="Built on top of React frontend, Node.js backend (with JWT authentication) and MongoDB database."
+        desc="React.js, Node.js, MongoDB, REST API, Heroku"
       />
+      <p className={classes.desc}>●	Developed a Node.js full-stack application with authentication that offers note-taking functionality for online users.</p>
+      <p className={classes.desc}>●	Wrote and authored theReact front-end, following best practices for user experience.</p>
+      <p className={classes.desc}>●	Constructed a MongoDB NoSQL database and hosted on Heroku PaaS to store users and notes.</p>
+      
     </Section>
     <Section id="skills" title="Skills">
       <Link

f4909213c11b7a8d8f6cbdce0147d6be9712e0b8
Noy JS <noy.agasi.na@gmail.com>
2023-06-18 18:59:49 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Mid Dev`,
+    title: `JS Dev`,
     description: `Full Stack Developer (Frontend oriented) based in Rishon LeZion, Israel.`,
     author: `Noy`,
   },
